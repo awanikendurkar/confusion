75ad3e95a552466d62be295f6d86ed6927d49a89
Awani <awanikendurkar@gmail.com>
2020-05-18 19:03:36 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbIte
     Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -108,8 +109,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
                     </Card>
                 </div>
             );
-        }
-        else {
+        } else {
             return (<div></div>);
         }
     }
@@ -146,9 +146,25 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
     }
 
     const Dishdetail = (props) => {
-        if (props.dish == null) {
+        if (props.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        } else if (props.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        } else if (props.dish == null) {
             return (<div></div>);
-        } else {
+        } else if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
